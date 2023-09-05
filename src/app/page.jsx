d0160f1f1e63d243c49f77eaa7d10920f14c7d70
Aayush kumar <aayushkumarhigh@gmail.com>
2023-09-05 21:07:22 +0530

    css changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import img from '../../public/pngwing.com.png'
 import Link from 'next/link'
 export default function Home() {
   return (<>
-    <main className=" bg-indigo-100 flex flex-col h-[92vh] justify-start  items-center">
+    <main id='mainpage' className=" bg-indigo-100 flex flex-col h-[92vh] justify-start  items-center">
       <p className=" mt-12 text-3xl">WELCOME</p>
       <p className="mt-2 text-xl">To</p>
       <p className=" mb-20 mt-2 text-2xl">My First Project</p>
